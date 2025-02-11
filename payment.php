04bf929f8122dd8bda0203e0ee67e9ef606748ce
Nataliia Batryniuk <nataliia.batryniuk@kpk-lp.com.ua>
2025-02-11 22:22:57 +0200

    Payment

--- payment.php
+++ payment.php
@@ -77,7 +77,7 @@ session_start();
       <!--Payment-->
       <section class = "my-5 py-5">
         <div class = "container text-center mt-3 pt-5">
-            <h2 class = "form-weight-bold">Check Out</h2>
+            <h2 class = "form-weight-bold">Payment</h2>
             <hr class = "mx-auto">
         </div>
         <div class = "mx-auto container text-center">
